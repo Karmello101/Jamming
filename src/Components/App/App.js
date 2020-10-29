c95a77447db9e017d8e273a56dd4d35a7324de68
Kevin <kevinarmella101@gmail.com>
2020-10-29 19:35:50 -0400

    Start the process of passing components to each other

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,16 +1,36 @@
 import React from 'react';
 import './App.css';
+import SearchResults from '../SearchResults/SearchResults';
+import SearchBar from '../SearchBar/SearchBar';
+import Playlist from '../Playlist/Playlist';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchResults: [
+        {name: 'name1' , artist: 'artist1', album: 'album1', id: 1},
+        {name: 'name2' ,artist: 'artist2 ', album: 'album2', id: 2},
+        {name: 'name3' ,artist: 'artist3 ', album: 'album3', id: 3}],
+        playlistName: 'My Playlist',
+        playlistTracks: [
+          {name: 'playlistname1' , artist: 'playlistartist1', album: 'playlistalbum1', id: 4},
+          {name: 'playlistname2' , artist: 'playlistartist2', album: 'playlistalbum2', id: 5},
+          {name: 'playlistname3' , artist: 'playlistartist3', album: 'playlistalbum3', id: 6}]
+    }
+  }
+
   render() {
     return (
       <div>
   <h1>Ja<span className="highlight">mmm</span>ing</h1>
   <div className="App">
-     {/* <SearchBar /> /*/} 
+      <SearchBar /> 
     <div className="App-playlist">
-    {/* <SearchResults /> */}
-      {/* <Playlist /> */}
+      <SearchResults searchResults={this.state.searchResults} />
+      <Playlist 
+      playlistName={this.state.playlistName} 
+      playlistTracks={this.state.playlistTracks}/>
     </div>
   </div>
 </div>
--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import './TrackList.css';
+import Track from '../Track/Track';
 
 class TrackList extends React.Component {
-  render() {
+  render () {
     return (
       <div className="TrackList">
-    {/*You will add a map method that renders a set of Track components */}
-</div>
+          {this.props.tracks &&
+           this.props.tracks.map(track => {
+              return <Track track={track}
+              key={track.id} />
+          })
+        }
+      </div>
     )
   }
 }
+
+export default TrackList;
