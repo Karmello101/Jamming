b358dd540d10504b50a8474a000e9f73b07471df
Kevin <kevinarmella101@gmail.com>
2020-11-01 20:22:17 -0500

    final touches and fixes finished project

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -3,38 +3,81 @@ import './App.css';
 import SearchResults from '../SearchResults/SearchResults';
 import SearchBar from '../SearchBar/SearchBar';
 import Playlist from '../Playlist/Playlist';
+import Spotify from '../../util/Spotify';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      searchResults: [
-        {name: 'name1' , artist: 'artist1', album: 'album1', id: 1},
-        {name: 'name2' ,artist: 'artist2 ', album: 'album2', id: 2},
-        {name: 'name3' ,artist: 'artist3 ', album: 'album3', id: 3}],
-        playlistName: 'My Playlist',
-        playlistTracks: [
-          {name: 'playlistname1' , artist: 'playlistartist1', album: 'playlistalbum1', id: 4},
-          {name: 'playlistname2' , artist: 'playlistartist2', album: 'playlistalbum2', id: 5},
-          {name: 'playlistname3' , artist: 'playlistartist3', album: 'playlistalbum3', id: 6}]
+      searchResults:[],
+      playlistName: 'My Playlist',
+      playlistTracks:[],
+    };
+    this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
+    this.updatePlaylistName = this.updatePlaylistName.bind(this);
+    this.savePlaylist = this.savePlaylist.bind(this);
+    this.search = this.search.bind(this);
+  }
+
+  addTrack(track) {
+    let tracks = this.state.playlistTracks;
+    if (tracks.find(savedTrack => savedTrack.id === track.id)) {
+      return;
+    } 
+      tracks.push(track);
+
+      this.setState({ playlistTracks: tracks });
     }
+
+  removeTrack(track) {
+    let tracks = this.state.playlistTracks;
+
+    tracks = tracks.filter(currentTrack => currentTrack.id !== track.id);
+
+    this.setState({ playlistTracks: tracks });
+  }
+
+  updatePlaylistName(name) {
+    this.setState({ playlistName: name })
+  }
+
+  savePlaylist() {
+    const trackUris = this.state.playlistTracks.map(track => track.uri);
+    Spotify.savePlaylist(this.state.playlistName, trackUris).then(() => {
+      this.setState(
+        { playlistName: 'New Playlist', 
+          playlistTracks: [] 
+        })
+    })
+}
+
+  search(term) {
+    Spotify.search(term).then(searchResults => {
+      this.setState({ searchResults : searchResults })
+    })
   }
 
   render() {
     return (
       <div>
-  <h1>Ja<span className="highlight">mmm</span>ing</h1>
-  <div className="App">
-      <SearchBar /> 
-    <div className="App-playlist">
-      <SearchResults searchResults={this.state.searchResults} />
+      <h1>Ja<span className="highlight">mmm</span>ing</h1>
+      <div className="App">
+        <SearchBar  onSearch={this.search} /> 
+      <div className="App-playlist">
+      <SearchResults 
+        searchResults={this.state.searchResults}
+        onAdd={this.addTrack} />
       <Playlist 
-      playlistName={this.state.playlistName} 
-      playlistTracks={this.state.playlistTracks}/>
+        playlistName={this.state.playlistName} 
+        playlistTracks={this.state.playlistTracks}
+        onRemove={this.removeTrack}
+        onNameChange={this.updatePlaylistName}
+        onSave={this.savePlaylist}/>
     </div>
   </div>
 </div>
-    )
+    );
   }
 }
 
--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -6,13 +6,16 @@ class TrackList extends React.Component {
   render () {
     return (
       <div className="TrackList">
-          {this.props.tracks &&
-           this.props.tracks.map(track => {
-              return <Track track={track}
-              key={track.id} />
-          })
-        }
-      </div>
+          {
+            this.props.tracks.map(track => {
+             return <Track track={track}
+              key={track.id}
+              onAdd={this.props.onAdd}
+              onRemove={this.props.onRemove} 
+              isRemoval={this.props.isRemoval}/>
+            })
+          }
+       </div>
     )
   }
 }
